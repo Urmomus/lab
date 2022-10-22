1b56d8076aabf4f27bcbe54e5a0129707c3b8d03
Chelique <yakov4enko.nikolay@yandex.ru>
2022-10-22 22:20:08 +0300

    refactored main function

--- calculate_series.cpp
+++ calculate_series.cpp
@@ -3,46 +3,56 @@
 using namespace std;
 
 
-int main()
+int main() // Начало программы
 {
-    setlocale(LC_ALL, "rus");
+    setlocale(LC_ALL, "rus"); // Подключение русского языка
 
-    int n, doubleFactorial;
-    double difference, currentSum, exactSum, x, eps, numerator;
+    int n; // Число итераций
+    int doubleFactorial; // Текущее значение удвоенного факториала
+    double difference; // Разность текущей и точной сумм
+    double currentSum; // Сумма
+    double exactSum; // Точное значение суммы
+    double x; // Переменная
+    double eps; // Точность
+    double numerator; // Числитель
 
     cout << "Введите точность: ";
-    cin >> eps;
-    cout << eps << endl;
+    cin >> eps; // Ввод точности
+    cout << eps << endl; // Эхо-печать
 
-    if (eps <= 0)
+    if (eps <= 0) // Валидация входящих данных
     {
-        cout << "Заданная точность должна быть больше нуля" << endl;
-        return 1;
+        cout << "Заданная точность должна быть больше нуля" << endl; // Вывод сообщения об ошибке
+        return 1; // Завершение работы программы со статусом 1 в случае некорректности введённых данных
     }
 
     cout << "Введите X: ";
-    cin >> x;
-    cout << x << endl;
+    cin >> x; // Ввод переменной x
+    cout << x << endl; // Эхо-печать
 
+    // Инициализация переменных
     n = 1;
     doubleFactorial = 1;
     currentSum = 1;
     numerator = 1;
 
-    exactSum = (exp(x) + exp(-x)) / 2;
-    difference = abs(currentSum - exactSum);
+    exactSum = (exp(x) + exp(-x)) / 2; // Подсчёт точной суммы
+    difference = abs(currentSum - exactSum); // Подсчёт разности
 
-    while (difference > eps)
+    while (difference > eps) // Начало цикла
     {
+        // Вычисление суммы ряда
         doubleFactorial = doubleFactorial * (2 * n) * (2 * n - 1);
         numerator = numerator * x * x;
         currentSum += numerator / doubleFactorial;
         n += 1;
         difference = abs(currentSum - exactSum);
-    }
+    } // Конец цикла
 
+    // Вывод значений переменных
     cout << "Сумма членов: " << currentSum << endl;
     cout << "Точное значение: " << exactSum << endl;
     cout << "Число слагаемых: " << n << endl;
     cout << "Разность суммы и точного значения: " << difference << endl;
+    return 0; // Возврат значения 0
 }
